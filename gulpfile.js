6a6cefa2cb294d131e13e29cba6c76d1e5b63395
KotoFeelGood72~ <kotenkolove@yandex.ru>
2022-12-23 00:35:27 +0300

    Complete standart work

--- gulpfile.js
+++ gulpfile.js
@@ -47,17 +47,17 @@ var assetsDir = 'assets/',
 	buildDir = 'build/';
 
 //--------------------------------------webp
-gulp.task('imgWebp', function () {
-	return gulp
-		.src(assetsDir + 'i/**/*')
-		.pipe(
-			webp({
-				quality: 80,
-			})
-		)
-		.pipe(gulp.dest(outputDir + 'i/'))
-		.pipe(browserSync.stream({ once: true }));
-});
+// gulp.task('imgWebp', function () {
+// 	return gulp
+// 		.src(assetsDir + 'i/**/*')
+// 		.pipe(
+// 			webp({
+// 				quality: 80,
+// 			})
+// 		)
+// 		.pipe(gulp.dest(outputDir + 'i/'))
+// 		.pipe(browserSync.stream({ once: true }));
+// });
 //--------------------------------------webp###
 
 //----------------------------------------------------Compiling
@@ -176,7 +176,7 @@ gulp.task('watch', function () {
 		gulp.series('jsConcatComponents')
 	);
 	gulp.watch(assetsDir + 'i/**/*', gulp.series('imageSync'));
-	gulp.watch(assetsDir + 'i/**/*', gulp.series('imgWebp'));
+	// gulp.watch(assetsDir + 'i/**/*', gulp.series('imgWebp'));
 	gulp.watch(
 		assetsDir + 'fonts/**/*',
 		gulp.series('fontsSync', 'fontsConvert')
@@ -367,7 +367,7 @@ let taskArray = {
 		gulp.parallel(
 			'pug',
 			'sass',
-			'imgWebp',
+			// 'imgWebp',
 			'imageSync',
 			'fontsSync',
 			'fontsConvert',
